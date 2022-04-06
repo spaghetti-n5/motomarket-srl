8cbd7a3224517150831ee80cdb6398c7a976dc3f
Eleonora <eleonora.baret@gmail.com>
2022-04-06 10:52:05 +0200

    added annunciToday and fixed bug in logo

--- src/components/Icons/Logo.jsx
+++ src/components/Icons/Logo.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import LogoIcon from '../../assets/icons/motomarket.jpeg';
 
 const Logo = () => (
-    <img src={LogoIcon} alt="logo" height="42px" width="100%" />
+    <img src={LogoIcon} alt="logo" height="42px" width="auto" />
 );
 
 export default Logo;
--- src/components/Shops/Shops.jsx
+++ src/components/Shops/Shops.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Delivery from '../../assets/images/delivery.jpg';
 import KijijiLogo from '../../assets/icons/kijiji-logo.png';
+import AnnunciTodayLogo from '../../assets/icons/annunci_today.png';
 
 const Shops = () => (
     <div className="onlineShopsSection">
@@ -13,9 +14,24 @@ const Shops = () => (
                 Visita il nostro rivenditore online per comprare comodamente da casa!
             </h2>
             <p className="p onlineShopsSection__description">
-                Sulla nostra pagina Kijiji potrai trovare gli ultimi prodotti ed offerte in anteprima, non lasciarteli scappare!
+                Nelle nostre pagine di Annunci Today e Kijiji potrai trovare gli ultimi prodotti ed offerte in anteprima, non lasciarteli scappare!
             </p>
             <div className="onlineShopsSection__itemWrapper">
+                <div className="onlineShopsSection__item">
+                    <a
+                        href="https://www.annuncitoday.it/utente/profilo/11721"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="onlineShopsSection__shopLink"
+                    >
+                        <img
+                            src={AnnunciTodayLogo}
+                            alt="annunci-today-logo"
+                            height="40"
+                            width="auto"
+                        />
+                    </a>
+                </div>
                 <div className="onlineShopsSection__item">
                     <a
                         href="https://www.kijiji.it/altri-annunci-utente/motomarket-srl-10523021"
@@ -26,8 +42,8 @@ const Shops = () => (
                         <img
                             src={KijijiLogo}
                             alt="kijiji-logo"
-                            height="52"
-                            width="52"
+                            height="40"
+                            width="40"
                         />
                     </a>
                 </div>
